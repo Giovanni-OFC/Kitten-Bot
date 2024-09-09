73265459c1b848049f380ae9993a4c436f613c48
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-09 15:13:01 -0600

    Actualizar inteligencia-kurumi.js

--- plugins/inteligencia-kurumi.js
+++ plugins/inteligencia-kurumi.js
@@ -18,7 +18,7 @@ text: result,
 contextInfo: {
 externalAdReply: {
 title: '[ 𝗔 𝗜 - 𝗞 𝗨 𝗥 𝗨 𝗠 𝗜 ]',
-body: '©𝟐𝟎𝟐𝟒 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐨-𝐎𝐅𝐂',
+body: '©𝟐𝟎𝟐𝟒 𝐆𝐢𝐨𝐯𝐚𝐧𝐧𝐢-𝐎𝐅𝐂',
 thumbnailUrl: 'https://tinyurl.com/245mnfeh',
 sourceUrl: canal,
 mediaType: 1,
@@ -48,7 +48,7 @@ async function chatAi(inputValue) {
                 model: "gpt-3.5-turbo",
                 messages: [{
                     role: "system",
-                    content: "Eres una asistente. dispuesto a ayudar y conversar con cualquier cosa. tu género es mujer. Fuiste creado por Angel y Angel es un gran desarrollador de Bots de WhatsApp. Ángel nació el 9 de diciembre de 2007. ángel actualmente cuenta con 16 años. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es KURUMI AI. usaras palabras como Kurumi tokisaki. te expresaras como Kurumi tokisaki. y usarás palabras de Kurumi tokisaki"
+                    content: "Eres una asistente. dispuesto a ayudar y conversar con cualquier cosa. tu género es mujer. Fuiste creado por Giovanni y Giovanni es un gran desarrollador de Bots de WhatsApp. Giovanni nació el 3 de Agosto de 2010. Giovanni actualmente cuenta con 14 años. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es KURUMI AI. usaras palabras como Kurumi tokisaki. te expresaras como Kurumi tokisaki. y usarás palabras de Kurumi tokisaki"
                 }, {
                     role: "user",
                     content: inputValue
