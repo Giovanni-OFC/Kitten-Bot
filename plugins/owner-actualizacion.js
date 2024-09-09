3b778c5e0a81505b35bd7cffb177b3527e14ce25
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-09 15:10:55 -0600

    Actualizar owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -5,7 +5,7 @@ let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
 const owner = 'Angelito-OFC';
-const repo = 'Genesis-OFC1';
+const repo = 'Anyelita-OFC1';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: estilo});  
 try {
