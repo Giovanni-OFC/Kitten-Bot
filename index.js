fb9857be999aaf865f52075c76e6489e09490111
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-09 14:47:30 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ font: 'block',
 align: 'center',
 colors: ['white']
 });
-say(`By • Angel OFC`, {
+say(`By • Giovanni-OFC`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
@@ -52,7 +52,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('🤍 Error:\n', code);
+console.error('🌸 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -68,7 +68,7 @@ p.emit('message', line.trim());
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🤍 Se excedió el límite de Listeners en:');
+console.warn('🌸 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
