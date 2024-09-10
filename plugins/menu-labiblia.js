86331b9be7fc70b8b5996ceaa04a8380ade34b0f
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-10 00:53:35 -0600

    Actualizar menu-labiblia.js

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -8,14 +8,14 @@ import fetch from 'node-fetch'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 const defaultMenu = {
-  before: `“𝐇𝐨𝐥𝐚 *%name* 𝐒𝐨𝐲 𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭, 𝐓𝐞𝐧 𝐮𝐧 𝐛𝐮𝐞𝐧 𝐝𝐢𝐚"
+  before: `“𝐇𝐨𝐥𝐚 *%name* 𝐒𝐨𝐲 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚𝐁𝐨𝐭, 𝐓𝐞𝐧 𝐮𝐧 𝐛𝐮𝐞𝐧 𝐝𝐢𝐚"
 
 ✧ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬ ✧ 
 
  '︶꒦꒷♡꒷꒦︶.
 ┊✶ 𝐂𝐥𝐢𝐞𝐧𝐭𝐞: %name
-┊┊✶ 𝐁𝐨𝐭: Génesis Bot
-┊┊✶ 𝐌𝐨𝐝𝐨: Público
+┊┊✶ 𝐁𝐨𝐭: Anyelita Bot
+┊┊✶ 𝐌𝐨𝐝𝐨: Privado
 ┊┊✶ 𝐅𝐞𝐜𝐡𝐚: ${fecha}
 ┊┊✶ 𝐔𝐬𝐞𝐫𝐬: %totalreg
 ┊┊✶ 𝐍𝐢𝐯𝐞𝐥: %level 
@@ -190,8 +190,8 @@ await m.react('🔞')
         text: text,
         contextInfo: {
         externalAdReply: {
-        title: '𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭-𝐌𝐃',
-        body: '©𝟐𝟎𝟐𝟒 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐨-𝐎𝐅𝐂',
+        title: '𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚𝐁𝐨𝐭-𝐌𝐃',
+        body: '©𝟐𝟎𝟐𝟒 𝐆𝐢𝐨𝐯𝐚𝐧𝐧𝐢-𝐎𝐅𝐂',
         thumbnailUrl: img,
         sourceUrl: global.canal,
         mediaType: 1,
