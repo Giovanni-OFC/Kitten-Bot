e1711a7d357925009d5ba9b7f64b231a3adca514
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-09 18:48:15 -0600

    Actualizar dl-ytmp3_Doc.js

--- plugins/dl-ytmp3_Doc.js
+++ plugins/dl-ytmp3_Doc.js
@@ -23,7 +23,7 @@ let thumbnail = await yt.thumbnail
 
 let img = await (await fetch(`${thumbnail}`)).buffer()  
 if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se cancelÃ³ la Descarga.`, m, rcanal).then(_ => m.react('✅'))
-await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '*By: GenesisBot*', mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: fkontak })
+await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '*By: AnyelitaBot*', mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: fkontak })
 await m.react('✅')
 } catch {
 await m.react('🕒')
