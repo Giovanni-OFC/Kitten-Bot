356435c8a93e8435452c0fc4549558f225b9073d
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-10 16:20:17 -0600

    Actualizar search-pinterest.js

--- plugins/search-pinterest.js
+++ plugins/search-pinterest.js
@@ -11,7 +11,7 @@ let handler = async (_0x10bd40, {
   command: _0x5ad406
 }) => {
   if (!_0x27db11) {
-    return _0x9c7141.reply(_0x10bd40.chat, "🤍 *¿Que quieres buscar en pinterest?*", _0x10bd40, rcanal);
+    return _0x9c7141.reply(_0x10bd40.chat, "🩷 *¿Que quieres buscar en pinterest?*", _0x10bd40, rcanal);
   }
     await _0x10bd40.react(rwait);
   async function _0x3f3fc7(_0x5f4723) {
@@ -70,7 +70,7 @@ let handler = async (_0x10bd40, {
         },
         'interactiveMessage': proto.Message.InteractiveMessage.fromObject({
           'body': proto.Message.InteractiveMessage.Body.create({
-            'text': "🤍 Resultado de : " + _0x27db11
+            'text': "🩷 Resultado de : " + _0x27db11
           }),
           'footer': proto.Message.InteractiveMessage.Footer.create({
             'text': "🔎 Pinterest - Busquedas"
