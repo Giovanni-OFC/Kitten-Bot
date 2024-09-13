dd63b40e7183e6625c9d2424c9095558edaa10e1
Jostin-444 <gemersport7@gmail.com>
2024-09-12 18:21:29 -0600

    Actualizar search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -38,7 +38,7 @@ deviceListMetadata: {},
 deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-body: proto.Message.InteractiveMessage.Body.create({ text: '🤍 *Resultado de:* ' + text }),
+body: proto.Message.InteractiveMessage.Body.create({ text: '🩷 *Resultado de:* ' + text }),
 footer: proto.Message.InteractiveMessage.Footer.create({ text: '🔎 Tiktok - Busquedas' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
