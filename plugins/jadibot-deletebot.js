fa5666cd56d1117698331ab2c573b91a6b5e8433
Jostin-444 <gemersport7@gmail.com>
2024-09-12 18:24:02 -0600

    Actualizar jadibot-deletebot.js

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -9,10 +9,10 @@ let userS = `${conn.getName(who)}`
 
 try {
 await fs.rmdir("./LuffyJadiBot/" + uniqid, { recursive: true, force: true })
-await parentw.sendMessage(m.chat, { text: '☁️ Sub-Bot eliminado.' }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: '🌸 Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
 if (err.code === 'ENOENT' && err.path === `./LuffyJadiBot/${uniqid}`) {
-await parentw.sendMessage(m.chat, { text: "🤍 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: "🩷 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
 } else {
 await m.react(error)
 }}}
