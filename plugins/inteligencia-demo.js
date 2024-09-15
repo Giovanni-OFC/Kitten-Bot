69aac121fdc4ed8a347d705beff9e74bfd835666
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-14 23:40:28 -0600

    Actualizar inteligencia-demo.js

--- plugins/inteligencia-demo.js
+++ plugins/inteligencia-demo.js
@@ -18,7 +18,7 @@ title: `[ 𝗖𝗛𝗔𝗧𝗚𝗣𝗧 - 𝗗𝗘𝗠𝗢 ]`,
 body: ``,
 "previewType": "PHOTO",
 thumbnailUrl: 'https://tinyurl.com/2awg2bch', 
-sourceUrl: 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'}}},
+sourceUrl: 'https://whatsapp.com/channel/0029VajfR79JENyA3ELFHf3o'}}},
 { quoted: m})
     } catch (err) {
         m.reply('error cik:/ ' + err);
