99d5c5ffdcf79cdbe33031972caf25e8286e33b4
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-15 22:23:43 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -8,16 +8,16 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `┌─★ 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚𝐁𝐨𝐭-𝐌𝐃\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Bienvenido a\n   │🩷  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let welcome = `┌─🎀 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚-𝐁𝐨𝐭\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Bienvenido/a a\n   │🩷  ${groupMetadata.subject}\n   └───────────────┈ᰔᩚ`
 await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, canal, estilo)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `┌─★ 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Se fue\n   │🩷 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let bye = `┌─🎀 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚-𝐁𝐨𝐭\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Se fue\n   │🩷 Jamás te quisimos aquí\n   └───────────────┈ლ`
 await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, canal, estilo)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `┌─★ 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Se fue\n   │🩷 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let kick = `┌─🎀 𝐀𝐧𝐲𝐞𝐥𝐢𝐭𝐚-𝐁𝐨𝐭\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩷  Se fue\n   │🩷 Jamás te quisimos aquí\n   └───────────────┈ᰔᩚ`
 await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, canal, estilo)
 }}
