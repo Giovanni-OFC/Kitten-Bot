8ffaa1ab76421d8667a08ff5fd391bbcb204c1e4
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-21 11:50:56 -0600

    Actualizar gc-advertencia.js

--- plugins/gc-advertencia.js
+++ plugins/gc-advertencia.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn, text, command, usedPrefix }) => {
 // if (m.mentionedJid.includes(conn.user.jid)) return; // Evitar advertir al bot mismo
-const pp = 'https://f.uguu.se/AVdSjfkk.jpg'
+const pp = 'https://i.ibb.co/vPyGy8F/file.jpg'
 let number, ownerNumber, aa, who;
 if (m.isGroup) { 
 who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text; 
