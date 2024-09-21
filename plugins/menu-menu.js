ff58e753605f5b2feb6e0ecf9e255cccc5a6198e
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-20 22:25:14 -0600

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -199,10 +199,10 @@ const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/
   global.vid = rlink
   const response = await fetch(vid)
   const gif = await response.buffer()
- // const img = { url: "https://qu.ax/ZlNo.jpg"} 
+ // const img = { url: "https://f.uguu.se/AVdSjfkk.jpg"} 
 
-await m.react('🩷') 
-await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲 𝗹𝗮 𝗕𝗼𝘁...𓏲੭*', fakegif3, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '🩷 ¡Anyelita la mejor Bot!', sourceUrl: "https://wa.me/0", thumbnail: icons }}})
+await m.react('🌸') 
+await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲 𝗹𝗮 𝗕𝗼𝘁...𓏲੭*', fakegif3, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '🌸 ¡Anyelita la mejor Bot!', sourceUrl: "https://wa.me/0", thumbnail: icons }}})
 
 // await conn.reply(m.chat, '🍟 Enviando el menú.....', m, rcanal)
 
