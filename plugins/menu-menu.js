2d9dd58c999c8d3430442ccf10539851969eee90
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-20 22:25:43 -0600

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -189,7 +189,7 @@ text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length
 
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
-const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/ZlNo.jpg')
+const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://f.uguu.se/AVdSjfkk.jpg')
 
   let category = "video"
   const db = './media/database/db.json'
