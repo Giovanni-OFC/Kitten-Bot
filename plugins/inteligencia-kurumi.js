6fb20203dca0490eebb12ad6b3ef7fd340e24171
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-22 09:22:35 -0600

    Actualizar inteligencia-kurumi.js

--- plugins/inteligencia-kurumi.js
+++ plugins/inteligencia-kurumi.js
@@ -9,7 +9,7 @@ let handler = async (m, {
     command
 }) => {
 
- if (!text) throw '➤ `𝗔𝗩𝗜𝗦𝗢` ♣️\n\n*PARA USAR ANYELITA IA*\n_Ejemplo: .iakurumi que sos?_';
+ if (!text) throw '➤ `𝗔𝗩𝗜𝗦𝗢` ♣️\n\n*PARA USAR KITTEN IA*\n_Ejemplo: .iakurumi que sos?_';
     await m.react('♣️');
     try {
         const result = await chatAi(text);
