bbd51ad42de3e61a88bdf42176aeb1740ae5eef6
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-22 23:00:31 -0600

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -206,7 +206,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 
 handler.help = ['menu']
 handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
+handler.command = ['menucompleto', 'menu2', 'allmenu'] 
 handler.register = true 
 export default handler
 
