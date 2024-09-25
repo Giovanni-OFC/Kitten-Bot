50b9f906339affdbb0e0e67e17999ed681459b65
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-24 20:09:14 -0600

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -15,7 +15,7 @@ const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isCha
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
 const used = process.memoryUsage()
 let yaemori = `╭─⬣「 *Estado De KittenBot* 」⬣\n`
-yaemori += `│ 🩷 *Creador ∙* Giovanni-OFC\n`
+yaemori += `│ 🌸 *Creador ∙* Giovanni-OFC\n`
 yaemori += `│ 📚 *Grupos Unidos ∙* ${groupsIn.length}\n`
 yaemori += `│ 👤 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`
 yaemori += `│ 💬 *Total De Chats ∙* ${chats.length}\n`
